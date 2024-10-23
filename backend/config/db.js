cf3d4b19bea032ab0388c3f3d4d5016cb93e98c0
VISHNU SHANKAR SINGH <104004983+VishnuShankarSingh2003@users.noreply.github.com>
2024-10-24 00:45:07 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb://localhost:27017/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://foodiejunction:foodiejunction123@cluster0.rw70l.mongodb.net/food-del').then(()=>console.log("DB Connected"));
    
 }
 
